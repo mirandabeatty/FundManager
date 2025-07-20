e710b048980bcde0024cebed2d770bacd7ca3fb1
ACD23MB <mbeatty1@sheffield.ac.uk>
2025-07-20 18:09:59 +0100

    Initial setup with SQLite and Spring Boot

---
+++ src/main/java/com/fundsim/investmentdashboard/InvestmentDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.fundsim.investmentdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fundsim/investmentdashboard/InvestmentDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundsim.investmentdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
